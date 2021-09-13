1472d6e256e791c9f9053258edc2fc629db8b501
GU NA HYUN <80106663+agunacoco@users.noreply.github.com>
2021-09-13 16:03:25 +0900

    2021.09.13

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,7 +17,13 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        // get 메소드를 사용하여 결과를 가져올 수 있다.
+        // $posts = Post::orderBy('updated_at', 'desc')->get();
+        
+        // latest 와 oldest 메소드는 여러준이 손쉽게 날짜를 기반으로 결과를 정렬. 
+        // 기본적으로 결과는 created_at 컬럼을 기준으로 정렬.
+        $posts = Post::latest('updated_at')->paginate(5);
+        
         return view('bbs.index', ['posts'=>$posts]);
     }
 
@@ -45,9 +51,23 @@ class PostsController extends Controller
             'content' => 'required|min:3',
         ]);
 
+        $filename = null;
+        // hasFile 메소드를 사용하면 request에 파일을 가지고 있는지 확인가능하다.
+        if($request->hasFile('image')){
+            // dd($request->file('image'));
+            $filename = time().'_'.$request->file('image')->getClientOriginalName();
+            // php artisan storage:link를 걸면 public 밑에 링크가 걸린다.
+            $path = $request->file('image')->storeAs('public/images', $filename);
+        }
         // array_merge는 배열끼리 합쳐서 새로운 배열을 생성해주는 함수.
-        $input = array_merge($request->all(), ['user_id'=> Auth::user()->id]);
+        $input = array_merge                                        ($request->all(), ['user_id'=> Auth::user()->id]);
+        if($filename){
+            $input = array_merge($input, ['image'=>$filename]);
+        }
         // create 메소드는 Post 모델 인스턴스를 생성하고 save() 메소드를 사용하여 데이터베이스에 저장한다.
+        // mass assignment
+        // Eloquent model의 white list인 $fillable에 기술해야 한다.
+        // post 모델에 정의된 $fillable에 정의된 컬럼만 사용가능하다.
         Post::create($input);
 
         // post방식으로 전달하면 view보다 redirect를 한다.
